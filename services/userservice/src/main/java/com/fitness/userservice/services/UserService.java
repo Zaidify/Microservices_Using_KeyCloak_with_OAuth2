64aa555a1f8e6f7a1af39d398108f1ee686d5613
zaidansari05 <zaidbingufran@gmail.com>
2025-09-01 12:34:14 +0530

    Demo project for Day2Day Coding

---
+++ services/userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -0,0 +1,53 @@
+package com.fitness.userservice.services;
+
+import com.fitness.userservice.UserRepository;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.dto.registerRequest;
+import com.fitness.userservice.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private UserRepository repository;
+
+    public UserResponse register(registerRequest request) {
+        if (repository.existsByEmail(request.getEmail())){
+                throw  new RuntimeException("Email already exists");
+        }
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+
+        User savedUser = repository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setPassword(savedUser .getPassword());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+
+    }
+
+    public UserResponse getUserProfile(String userId) {
+        User user = repository.findById(Long.valueOf(userId))
+                .orElseThrow(()->new RuntimeException("User not found"));
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setPassword(user .getPassword());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+    return userResponse;
+      }
+}
